0b2a58018dfd1ce87d9db6cb9c55db50303fc160
Aperence <anthony.doeraene@hotmail.com>
2024-08-27 20:42:17 +0200

    added customization for the saving delay

--- scripts/init.sh
+++ scripts/init.sh
@@ -16,7 +16,7 @@ tmux new-session -d -s server "./TerrariaServer.bin.x86_64 -config serverconfig.
 
 echo -n "" > /logs/logs
 
-(crontab -l 2> /dev/null ; echo "*/1 * * * * sh /home/save.sh") | sort - | uniq - | crontab -
+(crontab -l 2> /dev/null ; echo "*/$save_delay * * * * sh /home/save.sh") | sort - | uniq - | crontab -
 
 echo "Server ready ! Check logs/server.txt for information about the state of the server"
-crond -f&
+crond -f
